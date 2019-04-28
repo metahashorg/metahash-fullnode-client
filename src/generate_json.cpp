ff9bb9c682424583372a4be90e5df44725af8fcc
Andrey <temp00001@mail.ru>
2019-04-28 18:04:23 +0200

    added 'realFee' field in 'transactionInfoToJson'

--- src/generate_json.cpp
+++ src/generate_json.cpp
@@ -74,13 +74,14 @@ static rapidjson::Value transactionInfoToJson(const TransactionInfo &info, const
         infoJson.AddMember("timestamp", intOrString(bh.timestamp, isStringValue, allocator), allocator);
         infoJson.AddMember("type", strToJson(bh.getBlockType(), allocator), allocator);
         if (info.blockNumber == 0) {
-            infoJson.AddMember("blockNumber", intOrString(bh.blockNumber.value(), isStringValue, allocator), allocator);
+            infoJson.AddMember("blockNumber", intOrString(bh.blockNumber.value_or(0), isStringValue, allocator), allocator);
         } else {
             infoJson.AddMember("blockNumber", intOrString(info.blockNumber, isStringValue, allocator), allocator);
         }
         infoJson.AddMember("signature", strToJson(toHex(info.sign.begin(), info.sign.end()), allocator), allocator);
         infoJson.AddMember("publickey", strToJson(toHex(info.pubKey.begin(), info.pubKey.end()), allocator), allocator);
         infoJson.AddMember("fee", intOrString(info.fees, isStringValue, allocator), allocator);
+        infoJson.AddMember("realFee", intOrString(info.realFees.value_or(0), isStringValue, allocator), allocator);
         infoJson.AddMember("nonce", intOrString(info.nonce, isStringValue, allocator), allocator);
         if (info.intStatus.has_value()) {
             infoJson.AddMember("intStatus", info.intStatus.value(), allocator);
